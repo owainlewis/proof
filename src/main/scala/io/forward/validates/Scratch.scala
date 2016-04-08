19bb954cc410cf28befcfeb79978d3f67ef0e547
Owain Lewis <owain@owainlewis.com>
2016-04-08 17:45:53 +0100

    Update Scratch.scala

--- src/main/scala/io/forward/validates/Scratch.scala
+++ src/main/scala/io/forward/validates/Scratch.scala
@@ -9,7 +9,14 @@ class Scratch {
   def liftXorNel[A,B](xor: Xor[A,B]): Validated[NonEmptyList[A], B] = xor.toValidated.leftMap(NonEmptyList(_))
 
   def isValid[A,B](xor: Xor[A,B]) = xor.isRight
-
+ 
+ /**
+  * Combine multiple singular validations for some type T into a validation of type Validation[List[S],T]
+  * 
+  * This is used to combine multiple error outcomes into a single validation that either succeeds or contains
+  * a list of validation errors
+  * 
+  */
   def combine[S,T](obj: T, validations: List[Validation[S, T]]): Validation[List[S], T] = {
     val errors = validations.foldLeft(List.empty[S]) { case (xs, validation) =>
       validation match {
