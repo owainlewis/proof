410536c88eb5fcdcefdfb8fb69ed13e4ffdbda39
Owain Lewis <owain@owainlewis.com>
2016-04-09 17:45:07 +0100

    Update Implicits.scala

--- src/main/scala/io/forward/proof/Implicits.scala
+++ src/main/scala/io/forward/proof/Implicits.scala
@@ -3,16 +3,13 @@ package io.forward.proof
 import Validation._
 
 object Implicits {
-
+  implicit def toValidType[T](t: T): ValidType[T] = new ValidType(t)
   class ValidType[T](t: T) {
     def valid[S]: Validation[S,T] = Valid(t)
   }
   
-  implicit def toValidType[T](t: T): ValidType[T] = new ValidType(t)
-
+  implicit def toInvalidType[T](t: T): InvalidType[T] = new InvalidType(t)
   class InvalidType[S](t: S) {
     def invalid[T]: Validation[S,T] = Invalid(t)
   }
-  
-  implicit def toInvalidType[T](t: T): InvalidType[T] = new InvalidType(t)
 }
