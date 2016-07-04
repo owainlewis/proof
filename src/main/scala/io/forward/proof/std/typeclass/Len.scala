b62d62d8654b3576b881ecccf778a22df9e7627a
Owain Lewis <owain@owainlewis.com>
2016-07-04 19:53:05 +0100

    Update Len.scala

--- src/main/scala/io/forward/proof/std/typeclass/Len.scala
+++ src/main/scala/io/forward/proof/std/typeclass/Len.scala
@@ -1,9 +1,5 @@
 package io.forward.proof.std.typeclass
 
-/**
-  * A type class for things that have length i.e String, List etc
-  *
-  */
 trait Len[T] {
   def length(x: T): Int
 }
@@ -12,7 +8,6 @@ object Len {
   implicit def listLength[A]: Len[List[A]] = new Len[List[A]] {
     def length(x: List[A]) = x.length
   }
-
   implicit def stringLength[A]: Len[String] = new Len[String] {
     def length(x: String) = x.length
   }
